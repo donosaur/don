f8280323df4ed3353eb09f355fe1d443e6f480c8
Don Polistico <don@polisti.co>
2026-03-25 14:14:01 -0400

    mobile optimizations + misc fixes

---
+++ bs-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    server: {
+        baseDir: ".",
+        serveStaticOptions: {
+            extensions: ["html"]
+        }
+    },
+    files: ["**/*.html", "**/*.css", "**/*.js"],
+    port: 8000,
+    open: false,
+    notify: false
+};
